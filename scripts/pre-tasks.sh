1eed6ac59b032685be8e10918a111610255ffe95
Madson Ramos <madson.adm7@gmail.com>
2024-07-22 15:03:45 -0300

    🐛 Fix (Permission denied)

--- scripts/pre-tasks.sh
+++ scripts/pre-tasks.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-sudo rm /etc/sudoers.d/*
+sudo rm -rf /etc/sudoers.d/ || true
+sudo mkdir -p /etc/sudoers.d/
 
-cat << EOF > /etc/sudoers.d/madson
+sudo tee /etc/sudoers.d/$USER > /dev/null << EOF
 # User rules for $USER
 $USER ALL=(ALL) NOPASSWD:ALL
 EOF
@@ -24,13 +25,13 @@ VAULT_PASS_FILE=$(mktemp)
 echo "$PASS_VAULT" > "$VAULT_PASS_FILE"
 
 # ansible-vault encrypt
-ansible-vault decrypt ${PWD}/config/zsh/config-zsh/.export_rc --vault-password-file "$VAULT_PASS_FILE"
-ansible-vault decrypt ${PWD}/config/zsh/.zshrc --vault-password-file "$VAULT_PASS_FILE"
-ansible-vault decrypt ${PWD}/config/zsh/config-zsh/.alias_rc --vault-password-file "$VAULT_PASS_FILE"
-ansible-vault decrypt ${PWD}/config/zsh/config-zsh/.biglinux-zsh-config --vault-password-file "$VAULT_PASS_FILE"
-ansible-vault decrypt ${PWD}/config/zsh/config-zsh/.biglinux-zsh-prompt --vault-password-file "$VAULT_PASS_FILE"
-ansible-vault decrypt ${PWD}/config/zsh/config-zsh/.function_rc --vault-password-file "$VAULT_PASS_FILE"
-ansible-vault decrypt ${PWD}/config/ssh/id_rsa --vault-password-file "$VAULT_PASS_FILE"
-ansible-vault decrypt ${PWD}/config/ssh/id_rsa.pub --vault-password-file "$VAULT_PASS_FILE"
+ansible-vault decrypt ${PWD}/config/zsh/config-zsh/.export_rc --vault-password-file "$VAULT_PASS_FILE" || true
+ansible-vault decrypt ${PWD}/config/zsh/.zshrc --vault-password-file "$VAULT_PASS_FILE" || true
+ansible-vault decrypt ${PWD}/config/zsh/config-zsh/.alias_rc --vault-password-file "$VAULT_PASS_FILE" || true
+ansible-vault decrypt ${PWD}/config/zsh/config-zsh/.biglinux-zsh-config --vault-password-file "$VAULT_PASS_FILE" || true
+ansible-vault decrypt ${PWD}/config/zsh/config-zsh/.biglinux-zsh-prompt --vault-password-file "$VAULT_PASS_FILE" || true
+ansible-vault decrypt ${PWD}/config/zsh/config-zsh/.function_rc --vault-password-file "$VAULT_PASS_FILE" || true
+ansible-vault decrypt ${PWD}/config/ssh/id_rsa --vault-password-file "$VAULT_PASS_FILE" || true
+ansible-vault decrypt ${PWD}/config/ssh/id_rsa.pub --vault-password-file "$VAULT_PASS_FILE" || true
 
 rm "$VAULT_PASS_FILE"
