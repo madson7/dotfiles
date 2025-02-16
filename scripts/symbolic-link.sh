1980c365126cbd276b99eed970ae891a42914f88
Madson Ramos <madson.adm7@gmail.com>
2025-02-16 13:28:00 -0300

    (fix) ln

--- scripts/symbolic-link.sh
+++ scripts/symbolic-link.sh
@@ -18,20 +18,3 @@ ln -s ${PWD}/config/zsh/config-zsh/.function_rc ~/.config-zsh/.function_rc
 ln -s ${PWD}/config/ssh/id_rsa ~/.ssh/id_rsa
 ln -s ${PWD}/config/ssh/id_rsa.pub ~/.ssh/id_rsa.pub
 sudo ln -s ${PWD}/config/sys/sysctl.conf /etc/sysctl.conf
-
-rm -fr ~/.zshrc || true
-rm -fr ~/.config-zsh || true
-rm -fr ~/.ssh || true
-mkdir -p ~/.config-zsh || true
-mkdir -p ~/.ssh || true
-sudo rm /etc/sysctl.conf || true
-
-ln -s ${PWD}/config/zsh/.zshrc ~/.zshrc
-ln -s ${PWD}/config/zsh/config-zsh/.alias_rc ~/.config-zsh/.alias_rc
-ln -s ${PWD}/config/zsh/config-zsh/.biglinux-zsh-config ~/.config-zsh/.biglinux-zsh-config
-ln -s ${PWD}/config/zsh/config-zsh/.biglinux-zsh-prompt ~/.config-zsh/.biglinux-zsh-prompt
-ln -s ${PWD}/config/zsh/config-zsh/.export_rc ~/.config-zsh/.export_rc
-ln -s ${PWD}/config/zsh/config-zsh/.function_rc ~/.config-zsh/.function_rc
-ln -s ${PWD}/config/ssh/id_rsa ~/.ssh/id_rsa
-ln -s ${PWD}/config/ssh/id_rsa.pub ~/.ssh/id_rsa.pub
-sudo ln -s ${PWD}/config/sys/sysctl.conf /etc/sysctl.conf
